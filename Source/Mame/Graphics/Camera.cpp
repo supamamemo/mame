1981288f545afffbe8a085ddb869e868fe7d7a4f
mame0622 <mame.like.ww@gmail.com>
2023-06-09 10:16:26 +0900

    cameraクラスをまだ使い物になるくらいにはましになったよおお

--- Source/Mame/Graphics/Camera.cpp
+++ Source/Mame/Graphics/Camera.cpp
@@ -1,5 +1,7 @@
 #include "Camera.h"
 
+#include "../Input/Input.h"
+
 //// 透視投影
 //void Camera::Update(ID3D11DeviceContext* dc, DirectX::XMFLOAT3 eye, DirectX::XMFLOAT3 focus, DirectX::XMFLOAT3 up, ID3D11Buffer* CbSceneBuffer)
 //{
@@ -39,13 +41,13 @@ void Camera::SetPerspectiveFov(ID3D11DeviceContext* dc)
     dc->RSGetViewports(&unm_viewports, &viewport);
 
     float aspect_ratio{ viewport.Width / viewport.Height };
-    P={ DirectX::XMMatrixPerspectiveFovLH(DirectX::XMConvertToRadians(30),aspect_ratio,0.1f,100.0f) };
+    P = { DirectX::XMMatrixPerspectiveFovLH(DirectX::XMConvertToRadians(30),aspect_ratio,0.1f,100.0f) };
     
     
-    DirectX::XMVECTOR eye{ DirectX::XMVectorSet(pos.x,pos.y,pos.z,1.0f) };
-    DirectX::XMVECTOR focus{ DirectX::XMVectorSet(0.0f,0.0f,0.0f,1.0f) };
-    DirectX::XMVECTOR up{ DirectX::XMVectorSet(0.0f,1.0f,0.0f,0.0f) };
-    V={ DirectX::XMMatrixLookAtLH(eye, focus, up) };
+    DirectX::XMVECTOR eye{ DirectX::XMVectorSet(camera.eye.x,camera.eye.y,camera.eye.z,1.0f) };
+    DirectX::XMVECTOR focus{ DirectX::XMVectorSet(camera.focus.x,camera.focus.y,camera.focus.z,1.0f) };
+    DirectX::XMVECTOR up{ DirectX::XMVectorSet(camera.up.x,camera.up.y,camera.up.z,0.0f) };
+    V = { DirectX::XMMatrixLookAtLH(eye, focus, up) };
 
 
 }
@@ -53,6 +55,39 @@ void Camera::SetPerspectiveFov(ID3D11DeviceContext* dc)
 void Camera::DrawDebug()
 {
     ImGui::Begin("camera");
-    ImGui::DragFloat3("pos", &pos.x);
+
+    if (ImGui::TreeNode("camera_information"))
+    {
+        ImGui::DragFloat3("eye", &camera.eye.x);
+        ImGui::DragFloat3("focus", &camera.focus.x);
+        ImGui::DragFloat3("up", &camera.up.x);
+        ImGui::TreePop();
+    }
+
+    // todo : camera
+    // デバッグ用にカメラを動かしやすいようにしている
+    ImGui::DragFloat("camera_position.x", &camera.eye.x);
+    ImGui::DragFloat("camera_position.y", &camera.eye.y);
+    DebugMoveCamera();
+    if (ImGui::Button("Reset"))Camera::Reset();
+
     ImGui::End();
+}
+
+void Camera::Reset()
+{
+    camera.eye = { 0.0f,0.0f,-10.0f };
+}
+
+void Camera::DebugMoveCamera()
+{
+    GamePad& gamePad = Input::Instance().GetGamePad();
+
+    if (gamePad.GetButton() & GamePad::BTN_UP)camera.eye.y += 0.01f;
+    if (gamePad.GetButton() & GamePad::BTN_DOWN)camera.eye.y -= 0.01f;
+    if (gamePad.GetButton() & GamePad::BTN_RIGHT)camera.eye.x += 0.01f;
+    if (gamePad.GetButton() & GamePad::BTN_LEFT)camera.eye.x -= 0.01f;
+
+    camera.focus.x = camera.eye.x;
+    camera.focus.y = camera.eye.y;
 }
